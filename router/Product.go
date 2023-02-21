d72b3b1bd48518d67948d7ca4eef1403d10e4d56
Jadon <1258699382@qq.com>
2023-02-21 10:08:04 +0800

    拆分router

---
+++ router/Product.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"mall/controller"
+)
+
+func LoadProduct(r *gin.Engine) {
+
+	group := r.Group("/user")
+	{
+		//商品操作
+		group.GET("products", controller.ListProducts)
+		group.GET("product/:id", controller.ShowProduct)
+		group.POST("products", controller.SearchProducts)
+		group.GET("imgs/:id", controller.ListProductImg)   //商品图片
+		group.GET("categories", controller.ListCategories) //商品分类
+		group.GET("carousels", controller.ListCarousels)   //轮播图
+	}
+}
